34b5f7c32611baa637ffbc21ddf5df3825605450
AhmadMujtaba <cadeta6@gmail.com>
2023-02-07 07:26:53 +0500

    initital commit

---
+++ src/test/java/com/configuration/updated/UpdatedConfiguration2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configuration.updated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdatedConfiguration2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
